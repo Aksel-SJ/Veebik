b186a03c2aeffba82f77182292ce96a19b8d0a83
Aksel Nodapera <nodapera@ut.ee>
2024-12-02 19:19:24 +0200

    Muudatused JS failis

--- code/src/js/animations.js
+++ code/src/js/animations.js
@@ -1,7 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", () => {
+    /* Valib kõik elemdndi, millel on class "hidden"*/
     const hiddenElements = document.querySelectorAll('.hidden');
-
+    /*Funktsioon käivitub, siis kui kasutaja kerib lehel.
+      Kasutatakse selleks, et määrata kas iga peidetud element on kasutaja vaateväljas nähtav. */
     const onScroll = () => {
         hiddenElements.forEach((el) => {
             const rect = el.getBoundingClientRect();
@@ -14,8 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     };
-
+    /*Iga kord kui lehte kerida, onScroll funktsioon aktiveeritakse */
     window.addEventListener('scroll', onScroll);
 
     onScroll();
 });
+
+/*Kasutasin projekti Javascripti osa joaks ChatGPT'd(OpenAI, isiklik suhtlus, 02.12.2024). */
